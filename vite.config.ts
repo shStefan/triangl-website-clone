084a12a69db9942cc22a5d1687d6bd39856fa412
Stefan Shakhazizyan <stefanshakhazizyan@S-2.local>
2025-04-21 11:20:19 +0300

    Update vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/triangl-website/',
-}); 
+import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], base: "/triangl-website/" });
